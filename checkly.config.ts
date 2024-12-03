d8ca22303b03a9758cbfd789c44e68d7e6cbbe4a
tnolet <tim@checklyhq.com>
2024-12-03 14:12:17 +0100

    update config

--- checkly.config.ts
+++ checkly.config.ts
@@ -2,21 +2,20 @@ import { defineConfig } from 'checkly'
 
 const config = defineConfig({
   projectName: 'Axon workshop',
+  repoUrl: 'https://github.com/checkly/axon-workshop',
   logicalId: 'logical-id',
   checks: {
+    runtimeId: '2024.09',
     frequency: 10,
     locations: ['us-east-1', 'eu-west-1'],
-    runtimeId: '2024.02',
     checkMatch: '**/__checks__/**/*.check.ts',
-    playwrightConfig: {},
     browserChecks: {
       testMatch: '**/__checks__/**/*.spec.ts',
     },
   },
   cli: {
     runLocation: 'us-east-1',
-    reporters: ['list'],
-    retries: 0,
+    reporters: ['list']
   },
 })
 

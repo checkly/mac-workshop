dce7570f7376af0024b646081a83d1a12422fdff
tnolet <tim@checklyhq.com>
2024-12-04 10:16:13 +0100

    prep for publish

--- __checks__/6_envvariables.spec.ts
+++ __checks__/6_envvariables.spec.ts
@@ -3,10 +3,8 @@ import { test, expect } from '@playwright/test'
 test('test', async ({ page }) => {
   await page.goto('https://danube-web.shop/')
   await page.getByRole('button', { name: 'Log in' }).click()
-  await page.getByPlaceholder('Email').fill('user@email.com')
-//   await page.getByPlaceholder('Email').fill(process.env.USER_EMAIL as string)
-  await page.getByPlaceholder('Password').fill('supersecure1')
-//   await page.getByPlaceholder('Password').fill(process.env.USER_PASSWORD as string)
+  await page.getByPlaceholder('Email').fill(process.env.USER_EMAIL as string)
+  await page.getByPlaceholder('Password').fill(process.env.USER_PASSWORD as string)
   await page.getByRole('button', { name: 'Sign In' }).click()
   await expect(page.locator('#login-message'))
     .toContainText('Welcome back, user@email.com')
